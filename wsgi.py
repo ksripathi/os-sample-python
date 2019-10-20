323d91a10c54a9b34abf6e6679dfdd3c1d3c9599
sripathi <kammari.sripathi@gmail.com>
2019-10-20 11:33:33 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! changed"
+    return "Hello World! undo changed"
 
 if __name__ == "__main__":
     application.run()
